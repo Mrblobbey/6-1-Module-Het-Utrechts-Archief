0b1ffcd81ed5a03e53639a542d2e957ed34662df
Rody Stockschen <rody-100-@hotmail.com>
2025-12-08 09:02:28 +0100

    conecctie

--- HUA-website/includes/conn.php
+++ HUA-website/includes/conn.php
@@ -1,13 +1,13 @@
 <?php
-// $servername = "mysql_db";
-// $username = "root"; 
-// $password = "root";         
-// $dbname = "artikel";
-
-$servername = "localhost";
+$servername = "mysql_db";
 $username = "root"; 
-$password = "";         
+$password = "root";         
 $dbname = "artikel";
+
+// $servername = "localhost";
+// $username = "root"; 
+// $password = "";         
+// $dbname = "artikel";
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
